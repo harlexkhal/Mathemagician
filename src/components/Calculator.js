cb0daa7631790bfa7fdf3c99c893e9c8f7b0a118
Alexander Oguzie-Ibeh <harlexibeh01@gmail.com>
2022-03-16 10:17:35 +0100

    converted Calculator component to class and added componentDidMount life cycle

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Calculate from '../logic/Calculate';
 import Button from './Button';
 import Display from './Display';
 
-const Calculator = () => {
-  const [state, setState] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
+class Calculator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  const onBtnPressed = (btnName) => {
-    const result = Calculate(state, btnName);
-    setState(result);
+  componentDidMount() {
+    this.setState({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  }
+
+  onBtnPressed = (btnName) => {
+    const result = Calculate(this.state, btnName);
+    this.setState(result);
   };
 
-  const { total, next, operation } = state;
-  let buffer = `${total}${operation}${next}`.replace(/null/g, '');
-  buffer = buffer.replace(/undefined/g, '');
-  const window = buffer;
-  return (
-    <div className="main">
-      <Display display={window === '' ? undefined : window} />
-      <Button ctrls={['AC', '+/-', '%', '÷']} setBtn={onBtnPressed} last={false} />
-      <Button ctrls={['7', '8', '9', 'x']} setBtn={onBtnPressed} last={false} />
-      <Button ctrls={['4', '5', '6', '-']} setBtn={onBtnPressed} last={false} />
-      <Button ctrls={['1', '2', '3', '+']} setBtn={onBtnPressed} last={false} />
-      <Button ctrls={['0', '.', '=']} setBtn={onBtnPressed} last />
-    </div>
-  );
-};
+  render = () => {
+    const { total, next, operation } = this.state;
+    let buffer = `${total}${operation}${next}`.replace(/null/g, '');
+    buffer = buffer.replace(/undefined/g, '');
+    const window = buffer;
+
+    return (
+      <div className="main">
+        <Display display={window === '' ? undefined : window} />
+        <Button ctrls={['AC', '+/-', '%', '÷']} setBtn={this.onBtnPressed} last={false} />
+        <Button ctrls={['7', '8', '9', 'x']} setBtn={this.onBtnPressed} last={false} />
+        <Button ctrls={['4', '5', '6', '-']} setBtn={this.onBtnPressed} last={false} />
+        <Button ctrls={['1', '2', '3', '+']} setBtn={this.onBtnPressed} last={false} />
+        <Button ctrls={['0', '.', '=']} setBtn={this.onBtnPressed} last />
+      </div>
+    );
+  }
+}
 
 export default Calculator;
